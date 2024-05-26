b459a2c76daafefcf0ad7e21819e92880ed9bb1e
Ngocthu0401 <140692756+Ngocthu0401@users.noreply.github.com>
2024-05-26 22:56:36 +0700

    Add file upload

--- ecommerce/ecommerce/static/js/cart.js
+++ ecommerce/ecommerce/static/js/cart.js
@@ -17,7 +17,7 @@ for (i = 0; i < updateBtns.length; i++) {
 }
 
 function updateUserOrder(productId, action){
-	console.log('User is authenticated, sending data...')
+	console.log('User is authenticateds, sending data...')
 
 		var url = '/update_item/'
 
