dae1df85a7e5b2c2a2839b8d0ae35c8f9071b646
Tuan Loc <nghituanloc+github@gmail.com>
2024-06-13 01:11:18 +0700

    xoa cac file du

--- ecommerce/ecommerce/static/js/cart.js
+++ ecommerce/ecommerce/static/js/cart.js
@@ -17,7 +17,7 @@ for (i = 0; i < updateBtns.length; i++) {
 }
 
 function updateUserOrder(productId, action){
-	console.log('User is authenticateds, sending data...')
+	console.log('User is authenticated, sending data...')
 
 		var url = '/update_item/'
 
